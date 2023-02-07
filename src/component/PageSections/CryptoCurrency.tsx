d554a71edf794268f6a19f0c10a1018aa40dc41c
Aliu Salaudeen <ayokunumi99@gmail.com>
2023-02-07 18:05:20 +0100

    adjust animation

--- src/component/PageSections/CryptoCurrency.tsx
+++ src/component/PageSections/CryptoCurrency.tsx
@@ -35,41 +35,61 @@ const CryptoCurrency = () => {
 			<div className='flex flex-col lg:flex-row mt-[64px] gap-[45px]'>
 				{CRYPTOCURRENCIES.map((coin, idx) => (
 					<div
-						data-aos='flip-left'
-						data-aos-offset='270'
 						key={idx}
 						onClick={() => setSelected(idx)}
-						className={`${
-							selected === idx
-								? 'bg-[#2B076E] text-[#FFFF]'
-								: 'bg-white text-[#0D0D2B]'
-						}  card_shadow cursor-pointer w-full py-[48px] px-[24px] rounded-[16px]`}
+						className={`card_shadow cursor-pointer w-full overflow-hidden rounded-[16px]`}
 					>
-						<div className='mx-auto w-max mb-[48px]'>
-							<img src={coin.img} alt='' />
-						</div>
-						<h3 className='text-center capitalize'>
-							{coin.name}
-							<span className='font-normal uppercase ml-2 text-[18px] text-[#BDBDBD]'>
-								{coin.tag}
-							</span>
-						</h3>
-						<p className='text-center text-[#BDBDBD] mt-[12px]'>{coin.desc}</p>
-						{selected === idx ? (
-							<button className='bg-[#3671E9] mx-auto mt-[24px] flex gap-[24px] py-[16px] pr-[16px] pl-[24px] rounded-[32px]'>
-								Start mining
-								<img
-									src='/svgs/ArrowRight.svg'
-									alt=''
-									width={24}
-									height='24px'
-								/>
-							</button>
-						) : (
-							<div className='mx-auto mt-[24px] w-max'>
-								<img src='/svgs/right.svg' alt='' />
+						<div
+							className={`${
+								selected === idx
+									? 'bg-[#2B076E] text-[#FFFF]'
+									: 'bg-white text-[#0D0D2B]'
+							}  card_shadow cursor-pointer w-full h-full py-[48px] px-[24px]`}
+						>
+							<div
+								className={`${
+									selected === idx ? 'flip' : ''
+								} mx-auto w-max mb-[48px]`}
+							>
+								<img src={coin.img} alt='' />
 							</div>
-						)}
+							<h3
+								className={`${
+									selected === idx ? 'flip' : ''
+								} text-center capitalize`}
+							>
+								{coin.name}
+								<span
+									className={`${
+										selected === idx ? 'flip' : ''
+									} font-normal uppercase ml-2 text-[18px] text-[#BDBDBD]`}
+								>
+									{coin.tag}
+								</span>
+							</h3>
+							<p
+								className={`${
+									selected === idx ? 'flip' : ''
+								} text-center text-[#BDBDBD] mt-[12px]`}
+							>
+								{coin.desc}
+							</p>
+							{selected === idx ? (
+								<button className='bg-[#3671E9] mx-auto mt-[24px] flex gap-[24px] py-[16px] pr-[16px] pl-[24px] rounded-[32px]'>
+									Start mining
+									<img
+										src='/svgs/ArrowRight.svg'
+										alt=''
+										width={24}
+										height='24px'
+									/>
+								</button>
+							) : (
+								<div className='mx-auto mt-[24px] w-max'>
+									<img src='/svgs/right.svg' alt='' />
+								</div>
+							)}
+						</div>
 					</div>
 				))}
 			</div>
