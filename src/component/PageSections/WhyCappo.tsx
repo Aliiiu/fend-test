66f7536561a9ffc1f7cc748c9c1226a750f01225
Aliu Salaudeen <ayokunumi99@gmail.com>
2023-02-08 09:37:18 +0100

    updating favicon

--- src/component/PageSections/WhyCappo.tsx
+++ src/component/PageSections/WhyCappo.tsx
@@ -1,4 +1,75 @@
+import { useEffect, useState } from 'react';
+
 const WhyCappo = () => {
+	let [currency, setCurrency] = useState(0);
+	const [countries, setCountries] = useState(0);
+	let [wallets, setWallets] = useState(0);
+
+	// function counter(item, type, digit, time) {
+	// 	counts = item;
+	// 	item = setInterval(updated, time);
+	// 	let start = 0;
+
+	// 	function updated() {
+	// 		type.innerHTML = ++start;
+	// 		if (start === digit) {
+	// 			clearInterval(item);
+	// 		}
+	// 	}
+	// }
+	// function callCounter() {
+	// 	counter(counts, currency, 30, 60);
+	// 	counter(counts, wallets, 10, 200);
+	// 	counter(counts, countries, 195, 15);
+	// }
+
+	const startCounter = () => {
+		setInterval(() => {
+			if (currency <= 30) setCurrency((prevState) => prevState + 1);
+			if (wallets <= 10) setWallets((prevState) => prevState + 1);
+			if (countries <= 195) setCountries((prevState) => prevState + 1);
+		}, 0.1);
+	};
+	const stopCounter = () => {
+		if (currency >= 30) {
+			clearInterval(currency);
+		}
+		if (wallets >= 10) clearInterval(wallets);
+		if (countries >= 10) clearInterval(wallets);
+	};
+
+	useEffect(() => {
+		let start = 0;
+		window.addEventListener('load', () => {
+			const timer = setInterval(() => {
+				// if (currency <= 30) setCurrency(currency++);
+				setWallets(start++);
+				if (start === 10) {
+					console.log('yes', start);
+					clearInterval(timer);
+				}
+				// if (countries <= 195) setWallets((prevState) => prevState + 1);
+			}, 0.1);
+		});
+	}, []);
+	// useEffect(() => {
+	// 	window.addEventListener('load', () => {
+	// 		setInterval(() => {
+	// 			if (currency <= 30) setCurrency(currency++);
+	// 			if (wallets <= 10) setWallets((prevState) => prevState + 1);
+	// 			if (countries <= 195) setWallets((prevState) => prevState + 1);
+	// 		}, 0.1);
+	// 	});
+
+	// 	return () =>
+	// 		window.removeEventListener('load', () => {
+	// 			if (currency >= 30) {
+	// 				clearInterval(currency);
+	// 			}
+	// 			if (wallets >= 10) clearInterval(wallets);
+	// 			if (countries >= 195) clearInterval(countries);
+	// 		});
+	// }, []);
 	return (
 		<section className='container relative px-4 md:px-[40px] overflow-hidden lg:px-[60px] lg:pt-[60px] xl:px-[120px] xl:pt-[100px]'>
 			<img
@@ -29,7 +100,7 @@ const WhyCappo = () => {
 				>
 					<img src='/svgs/totalFX.svg' alt='' />
 					<div className='flex flex-col gap-[4px]'>
-						<h3 className=''>$30B</h3>
+						<h3 className=''>30B</h3>
 						<p className='text-[#E0E0E0]'>Digital Currency Exchanged</p>
 					</div>
 				</div>
@@ -51,7 +122,7 @@ const WhyCappo = () => {
 				>
 					<img src='/svgs/countries.svg' alt='' />
 					<div className='flex flex-col gap-[4px]'>
-						<h3 className=''>195</h3>
+						<h3 className=''>193</h3>
 						<p className='text-[#E0E0E0]'>Countries Supported</p>
 					</div>
 				</div>
